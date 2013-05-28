b567ee681cfce626a4edddb15e2e9743c9932253
Stanley Gu <stanleygu@gmail.com>
2013-05-28 08:32:12 -0700

    corrected routing

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 8000);
+app.set('port', process.env.PORT || 8002);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -29,7 +29,7 @@ if ('development' == app.get('env')) {
 
 //app.get('/', routes.index);
 app.get('/users', user.list);
-app.all('/bives', routes.bives)
+app.all('/', routes.bives)
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
